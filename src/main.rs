2fedb3407be6df3ca8f982377bd6339102793ef1
thembones79 <michal.szumnarski@gmail.com>
2025-01-01 14:28:42 +0100

    rust iterators

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,11 @@ fn print_elements2(elements: &[String]) {
         .map(|el| format!("{} {}", el, el))
         .for_each(|el| println!("{}", el));
 }
+fn shorten_strings(elements: &mut Vec<String>) {
+    elements.iter_mut().for_each(|el| el.truncate(1));
+}
 fn main() {
-    let colors = vec![
+    let mut colors = vec![
         String::from("red"),
         String::from("green"),
         String::from("blue"),
@@ -24,5 +27,6 @@ fn main() {
     // println!("{:#?}", colors_iter.next());
 
     print_elements(&colors);
+    shorten_strings(&mut colors);
     print_elements2(&colors);
 }
