7580294d3ce1c431da6c4a841be7dfbb834654c2
thembones79 <michal.szumnarski@gmail.com>
2025-01-08 16:55:40 +0100

    rust iterators

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,9 @@ fn shorten_strings(elements: &mut [String]) {
 fn to_uppercase(elements: &[String]) -> Vec<String> {
     elements.iter().map(|el| el.to_uppercase()).collect()
 }
+fn move_elements(vec_a: Vec<String>, vec_b: &mut Vec<String>) {
+    vec_a.into_iter().for_each(|el| vec_b.push(el));
+}
 fn main() {
     let mut colors = vec![
         String::from("red"),
@@ -29,9 +32,13 @@ fn main() {
     // println!("{:#?}", colors_iter.next());
     // println!("{:#?}", colors_iter.next());
 
-    print_elements(&colors);
-    let uppercased = to_uppercase(&colors);
-    println!("{:#?}", uppercased);
-    shorten_strings(&mut colors);
-    print_elements2(&colors);
+    // print_elements(&colors);
+    // let uppercased = to_uppercase(&colors);
+    // println!("{:#?}", uppercased);
+    // shorten_strings(&mut colors);
+    // print_elements2(&colors);
+
+    let mut destination = vec![];
+    move_elements(colors, &mut destination);
+    println!("Colors: {:#?}, Destination: {:#?}", &destination, destination);
 }
